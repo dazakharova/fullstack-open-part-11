39dd109315abec63fb9820689f5a7b81d22b10d5
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-10-24 16:07:33 +0300

    Update eslint config for e2e tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,21 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  settings: {
-    react: { version: 'detect' }
-  },
+  env: { browser: true, es2021: true, node: true },
+  settings: { react: { version: 'detect' } },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: { jsx: true }
+    ecmaFeatures: { jsx: true },
   },
   plugins: ['react', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
   ],
   rules: {
-    'react/react-in-jsx-scope': 'off', // React 17+ no need to import React
-    'react/prop-types': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
   overrides: [
     {
@@ -35,9 +29,16 @@ module.exports = {
         beforeAll: 'readonly',
         afterAll: 'readonly',
         beforeEach: 'readonly',
-        afterEach: 'readonly'
-      }
-    }
+        afterEach: 'readonly',
+      },
+    },
+    {
+      files: ['e2e/**/*.{js,ts}'],
+      extends: ['plugin:playwright/recommended'],
+      plugins: ['playwright'],
+      rules: { 'playwright/no-conditional-in-test': 'off' },
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    },
   ],
-  ignorePatterns: ['dist/', 'node_modules/', 'playwright-report/']
+  ignorePatterns: ['dist/', 'node_modules/', 'playwright-report/'],
 }
